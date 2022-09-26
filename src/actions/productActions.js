331758b4c091b07f2127b5cdceb79fa5c9ef4d4d
unknown <amandeeppos@gmail.com>
2022-09-26 15:57:23 +0530

    changed linksproduct

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -15,13 +15,13 @@ export const getProducts = (keyword = ' ', currentPage = 1, price, category, rat
     try {
         dispatch({ type: ALL_PRODUCTS_REQUEST })
 
-        let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}
+        let link = `https://mjjewelers.herokuapp.com/api/v1/products?keyword=${keyword}&page=${currentPage}
                     &price[lte]=${price[1]}&price[gte]=${price[0]}&ratings[gte]=${rating}`
 
 
         if (category) {
             link =
-                `/api/v1/products?keyword=${keyword}&page=${currentPage}'
+                `https://mjjewelers.herokuapp.com/api/v1/products?keyword=${keyword}&page=${currentPage}'
                     &price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}&ratings[gte]=${rating}`
 
 
@@ -52,7 +52,7 @@ export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST })
 
-        const { data } = await axios.get(`/api/v1/product/${id}`)
+        const { data } = await axios.get(`https://mjjewelers.herokuapp.com/api/v1/product/${id}`)
         console.log(data)
 
         dispatch({
